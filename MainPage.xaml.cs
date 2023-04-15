7bc1e4ab12d9cfc705a3689ded83bd1ab449351e
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-04-15 16:42:29 +0545

    update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,8 +7,8 @@ namespace TextCompressionApp;
 
 public partial class MainPage : ContentPage
 {
-    string originalFile = "D:\\Projects\\Realtime  Development\\App\\TextCompressionApp\\Huffman Files\\original.txt";
-    string encodedFile = "D:\\Projects\\Realtime  Development\\App\\TextCompressionApp\\Huffman Files\\encoded.txt";
+    string encodedFileForHuffman = "D:\\Projects\\Realtime  Development\\App\\TextCompressionApp\\Huffman Files\\huffmanencoded.txt";
+    string encodedFileForLZW = "D:\\Projects\\Realtime  Development\\App\\TextCompressionApp\\Huffman Files\\lzwencoded.txt";
     string decodedFile = "D:\\Projects\\Realtime  Development\\App\\TextCompressionApp\\Huffman Files\\decoded.txt";
 
     public MainPage()
@@ -53,7 +53,7 @@ public partial class MainPage : ContentPage
         //saving bytes to file
         byte[] encodedBytes = new byte[encoded.Length / 8 + (encoded.Length % 8 == 0 ? 0 : 1)];
         encoded.CopyTo(encodedBytes, 0);
-        await File.WriteAllBytesAsync(encodedFile, encodedBytes);
+        await File.WriteAllBytesAsync(encodedFileForHuffman, encodedBytes);
 
         BitArray decodedBits = new BitArray(encodedBytes);
         BitArray neededDecodedBits = new BitArray(encoded.Length);
@@ -72,7 +72,7 @@ public partial class MainPage : ContentPage
         infoLabel.Text = "Writing decoded Text...";
         await File.WriteAllTextAsync(decodedFile, decoded);
         streamReader.Close();
-        long encodedFileSize = new FileInfo(encodedFile).Length;
+        long encodedFileSize = new FileInfo(encodedFileForHuffman).Length;
 
         encodedTimeForHuffman.Text = $"Compressed Time: {encodeTime} ms";
         decodedTimeForHuffman.Text = $"DeCompressed Time: {decodeTime} ms";
@@ -115,10 +115,10 @@ public partial class MainPage : ContentPage
         BitArray encoded = new BitArray(encodedBoolean.ToArray());
         byte[] encodedBytes = new byte[encoded.Count / 8 + (encoded.Count % 8 == 0 ? 0 : 1)];
         encoded.CopyTo(encodedBytes, 0);
-        File.WriteAllBytes(encodedFile, encodedBytes);
+        File.WriteAllBytes(encodedFileForLZW, encodedBytes);
 
         // Decode
-        byte[] readByte = await File.ReadAllBytesAsync(encodedFile);
+        byte[] readByte = await File.ReadAllBytesAsync(encodedFileForLZW);
         BitArray decodedBits = new BitArray(readByte);
         string decodedString = "";
         List<int> deCompressedList = new List<int>();
@@ -147,7 +147,7 @@ public partial class MainPage : ContentPage
         await File.WriteAllTextAsync(decodedFile, decompressed);
         streamReader.Close();
 
-        long encodedFileSize = new FileInfo(encodedFile).Length;
+        long encodedFileSize = new FileInfo(encodedFileForLZW).Length;
         encodedTimeForLZW.Text = $"Compressed Time: {encodeTime}";
         decodedTimeForLZW.Text = $"DeCompressed Time: {decodeTime}";
         originalFileSizeForLZW.Text = $"Original File Size: {(float)originalFileSize / 1024} Kb";
